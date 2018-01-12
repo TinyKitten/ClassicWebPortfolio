44e4b616ddb56aac75195887b29de245fc2adae5
TinyKitten <catwalk8205@gmail.com>
2018-01-12 18:01:20 +0900

    モワモワしすぎてキモい

--- components/share.vue
+++ components/share.vue
@@ -53,7 +53,7 @@ Twitter
   border-radius: 8px;
   margin-top: 48px;
   font-weight: bold;
-  box-shadow: 0 0 12px rgba(0, 0, 0, .25);
+  box-shadow: 0 0 8px rgba(0, 0, 0, .25);
   transition: .25s;
 }
 
