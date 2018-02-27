05e4f3bf369c5ad9c85f89948144ae1b5821cb96
TinyKitten <catwalk8205@gmail.com>
2018-02-27 22:27:29 +0900

    一部アニメーション廃止とフッター

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <page-header />
     <works />
     <about-me />
+    <footer class="footer">
+      <span class="copyright">Copyright &copy; 2018 TinyKitten</span>
+    </footer>
   </section>
 </template>
 
@@ -23,5 +26,17 @@ export default {
 <style scoped>
   .container {
     height: 100%;
+  }
+  .footer {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    text-align: center;
+    height: 64px;
+    color: #aaa;
+    line-height: 1.5rem;
+    font-family: sans-serif;
   }
 </style>
